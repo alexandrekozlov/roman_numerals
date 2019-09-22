5ea103bb5f8c5030d2680d87128970af006cc28c
Alex Kozlov <alexandre.kozlov@abcellera.com>
2019-09-22 00:44:10 -0600

    First version.

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+
+#include "catch.hpp"
+#include "roman.h"
+
+
+TEST_CASE( "Life the universe and everything", "[hhgttg]" )
+{
+    SECTION("Test") {
+    REQUIRE( to_roman(1) == "I" );
+    REQUIRE( to_roman(2) == "II" );
+    REQUIRE( to_roman(3) == "III" );
+    REQUIRE( to_roman(4) == "IV" );
+    REQUIRE( to_roman(5) == "V" );
+    REQUIRE( to_roman(6) == "VI" );
+    REQUIRE( to_roman(7) == "VII" );
+    REQUIRE( to_roman(8) == "VIII" );
+    REQUIRE( to_roman(9) == "IX" );
+
+    REQUIRE( to_roman(10) == "X" );
+    REQUIRE( to_roman(20) == "XX" );
+    REQUIRE( to_roman(30) == "XXX" );
+    REQUIRE( to_roman(40) == "XL" );
+    REQUIRE( to_roman(50) == "L" );
+    REQUIRE( to_roman(60) == "LX" );
+    REQUIRE( to_roman(70) == "LXX" );
+    REQUIRE( to_roman(80) == "LXXX" );
+    REQUIRE( to_roman(90) == "XC" );
+    
+    REQUIRE( to_roman(100) ==    "C" );
+    REQUIRE( to_roman(200) ==   "CC" );
+    REQUIRE( to_roman(300) ==  "CCC" );
+    REQUIRE( to_roman(400) ==   "CD" );
+    REQUIRE( to_roman(500) ==    "D" );
+    REQUIRE( to_roman(600) ==   "DC" );
+    REQUIRE( to_roman(700) ==  "DCC" );
+    REQUIRE( to_roman(800) == "DCCC" );
+    REQUIRE( to_roman(900) ==   "CM" );
+
+    REQUIRE( to_roman(1000) == "M" );
+    REQUIRE( to_roman(2000) == "MM" );
+    REQUIRE( to_roman(3000) == "MMM" );
+    REQUIRE( to_roman(4000) == "MMMM" );
+    REQUIRE( to_roman(5000) == "MMMMM" );
+
+    REQUIRE( to_roman(1990) == "MCMXC" );
+    REQUIRE( to_roman(2008) == "MMVIII" );
+    REQUIRE( to_roman(99) == "XCIX" );
+    REQUIRE( to_roman(47) == "XLVII" );
+    REQUIRE( to_roman(42) == "XLII" );
+    }
+
+    SECTION("Benchmark") {
+        BENCHMARK("1 digit") {
+            return to_roman(1);
+        };
+
+        BENCHMARK("long") {
+            return to_roman(1973);
+        };
+
+        BENCHMARK("full") {
+            return to_roman(1888);
+        };
+
+        BENCHMARK("extreme") {
+            return to_roman(5573);
+        };
+    }
+}
---
+++ roman.cpp
@@ -0,0 +1,89 @@
+#include "roman.h"
+#include <cinttypes>
+#include <string>
+
+#if !SLOW_BUT_READABLE
+
+constexpr alignas(64) const char* romand_decades[3][10]{
+    {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+    {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+    {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+};
+
+std::string to_roman(unsigned int value) noexcept
+{
+    if (value == 0)
+        return "nulla";
+
+    std::string s;
+
+    auto milla = value / 1000;
+    for (auto i = 0u; i < milla; ++i)
+        s += 'M';
+
+    value %= 1000;
+    auto centum = value / 100;
+    if (centum != 0)
+        s += romand_decades[2][centum];
+
+    value %= 100;
+    auto decem = value / 10;
+    if (decem != 0)
+        s += romand_decades[1][decem];
+
+    value %= 10;
+    if (value != 0)
+        s += romand_decades[0][value];
+
+    return s;
+}
+
+#else
+
+static alignas(64) constexpr char kRomanDigits[]{"IVIIIXLXXXCDCCCM"};
+
+static alignas(64) constexpr std::pair<uint8_t, uint8_t> kRomanGroupsIndex[]{
+    {0, 0},  {0, 1},  {2, 2},  {2, 3},  {0, 2},  {1, 1},  {1, 2},  {1, 3},
+    {1, 4},  {4, 2},  {0, 0},  {7, 1},  {7, 2},  {7, 3},  {5, 2},  {6, 1},
+    {6, 2},  {6, 3},  {6, 4},  {9, 2},  {0, 0},  {12, 1}, {12, 2}, {12, 3},
+    {10, 2}, {11, 1}, {11, 2}, {11, 3}, {11, 4}, {14, 2},
+};
+
+std::string to_roman(unsigned int value) noexcept
+{
+    if (value == 0)
+        return "nulla";
+
+    std::string s;
+
+    auto milla = value / 1000;
+    if (milla > 16)
+        s.reserve(milla + 3 * 4);
+
+    s.append(milla, 'M');
+
+    value %= 1000;
+    auto centum = value / 100;
+    if (centum != 0) {
+        auto p = kRomanGroupsIndex[20 + centum];
+        s.append(&kRomanDigits[p.first], p.second);
+    }
+
+    value %= 100;
+    auto decem = value / 10;
+    if (decem != 0) {
+        auto p = kRomanGroupsIndex[10 + decem];
+        s.append(&kRomanDigits[p.first], p.second);
+    }
+
+    value %= 10;
+    if (value != 0) {
+        auto p = kRomanGroupsIndex[value];
+        s.append(&kRomanDigits[p.first], p.second);
+    }
+
+    return s;
+}
+
+
+#endif
